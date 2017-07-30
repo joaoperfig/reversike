9891c655a835cbc47eb1f7ad391592ccfd825324
joaoperfig <joaoperfig@gmail.com>
2017-07-30 20:46:47 +0100

    generator working

--- source/generator.py
+++ source/generator.py
@@ -3,9 +3,13 @@ from os import path
 from gtts import gTTS
 from pydub import AudioSegment
 from pydub.playback import play
+from mikerar_for_rev import memory
+from mikerar_for_rev import wsort
+import mikerar_for_rev as mikerar
+import random
 
 def test():
-    tts = gTTS("blackbird", lang='en')
+    tts = gTTS("Shay has grown harvested.", lang='en')
     tts.save("gtemp.mp3")    
     audio = AudioSegment.from_file("gtemp.mp3")
     audio = audio.reverse()
@@ -28,5 +32,115 @@ def test():
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
         
+# rselect RandomSelect returns random element of list
+def rselect(lista):
+    return random.choice(lista)
 
 
+# wselect WeightedSelect returns element of dictionary based on dict weights {element:weight}
+def wselect(dicti):
+    total=0
+    for i in list(dicti):
+        total = total + dicti[i]
+    indice = total*random.random()
+    for i in list(dicti):
+        if dicti[i]>=indice:
+            return i
+        indice = indice - dicti[i]
+    raise ValueError ("something went wrong")
+   
+def tempFile():
+    return "../temp/gtemp.mp3"
+        
+def flacFile():
+    return "../temp/gtemp.flac"
+
+def interpRev(sent):
+    tts = gTTS(sent, lang='en')
+    tts.save(tempFile())      
+    audio = AudioSegment.from_file(tempFile())
+    audio = audio.reverse()    
+    audio.export(flacFile(), format = "flac")
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), flacFile())
+    
+    r = sr.Recognizer()           #Generate the files for interpretation
+    with sr.AudioFile(AUDIO_FILE) as source:
+        audio = r.record(source)  # read the entire audio file     
+        
+    try:                          #Interpret through google's api
+        interp = r.recognize_google(audio)
+    except sr.UnknownValueError:
+        interp = ""
+    except sr.RequestError as e:
+        interp = ""
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))        
+    return interp
+        
+def runOnce():
+    size = wselect({1:4, 2:5, 3:4, 4:3, 5:1})
+    sent = mikerar.generateSent(size)
+    
+    interp = interpRev(sent) 
+    if interp == "":
+        return (sent, "", "", 0) #sentence, interpretation, reinterp score
+    
+    reinterp = interpRev(interp)
+    rescore = maxComScore(sent, reinterp)/len(sent)
+
+    print(sent + " --> " + interp + " <-- " + reinterp + " # "+str(rescore))
+    return (sent, interp, reinterp, rescore)
+    
+def iterateAndFile(count=10):
+    for i in range(count):
+        this = runIterations(400,30)
+        f = open("../resources/itandf2_"+str(i)+".txt", "w")
+        for j in this:
+            f.write(j[0] + " -> " + j[1] + " <- " + j[2] + " #" + str(j[3])+"\n")
+        f.close()
+    
+def runIterations(count=200, topsize=10):
+    top = ()
+    counter = 0
+    for i in range(count):
+        print(str((counter/count)*100) +"%")
+        counter = counter+1
+        res = runOnce()
+        if res[3] > 0:
+            put = False
+            for i in range(len(top)):
+                if res[3]>top[i][3]:
+                    top = top[:i] + (res,) + top[i:]
+                    put = True
+                    break
+            if not put:
+                top = top + (res,)
+            top = top[:topsize]
+    return top
+                
+def maxComScore(sent1, sent2):
+    if len(sent1) == 0 or len(sent2) == 0:
+        return 0
+    l = longestComSub(sent1, sent2)
+    if len(l) == 0:
+        return 0
+    i1 = sent1.index(l)
+    i2 = sent2.index(l)
+    first1 = sent1[:i1]
+    first2 = sent2[:i2]
+    second1 = sent1[i1+len(l):]
+    second2 = sent2[i2+len(l):]
+    return len(l) + maxComScore(first1, first2) + maxComScore(second1, second2)
+    
+def longestComSub(s1, s2): #not my stuff, got it online
+    m = [[0] * (1 + len(s2)) for i in range(1 + len(s1))]
+    longest, x_longest = 0, 0
+    for x in range(1, 1 + len(s1)):
+        for y in range(1, 1 + len(s2)):
+            if s1[x - 1] == s2[y - 1]:
+                m[x][y] = m[x - 1][y - 1] + 1
+                if m[x][y] > longest:
+                    longest = m[x][y]
+                    x_longest = x
+            else:
+                m[x][y] = 0
+    return s1[x_longest - longest: x_longest]
--- source/mikerar_for_rev.py
+++ source/mikerar_for_rev.py
@@ -3,6 +3,7 @@ import gzip
 import shutil
 import os
 import time
+import random
 
 class wsort:
     def __init__(self):
@@ -29,6 +30,21 @@ class wsort:
                 index = index-1
             else:
                 return
+    def toDict(self):
+        dic = {}
+        for i in self._list:
+            dic[i[0]] = i[1]
+        return dic
+    def wselect(self): #faster than wselect(toDict(wsort))
+        count = 0
+        for i in self._list:
+            count = count + i[1]
+        selecter = count*random.random()
+        for i in self._list:
+            if i[1] >= selecter:
+                return i[0]
+            selecter = selecter-i[1]
+        raise ValueError("Somethin went wrong")
 
 class memory:
     def __init__(self):
@@ -47,6 +63,25 @@ class memory:
             new = wsort()
             new.incIndex(second)
             self._wordsort[first] = new
+            
+def generateSent(size, returnList = False):
+    data = getData()
+    last = data._wordsort["%#,start"].wselect()
+    words = (last,)
+    while len(words)<size:
+        try:
+            last = data._wordsort[last].wselect()
+        except:
+            last = data._mainsort.wselect()
+        words = words + (last,)
+    if returnList:
+        return words
+    string = words[0][0].capitalize() + words[0][1:]
+    for i in words[1:]:
+        string = string + " " + i
+    string = string + "."
+    return string
+    
 
 def uncap(word):
     return (" " + word).capitalize()[1:]
@@ -158,34 +193,6 @@ def partition(alist,first,last):
     alist[rightmark] = temp
     return rightmark
 
-def transmike(filename):
-    source = "transrar/hp1_en.pi"
-    dest = "transrar/hp1_pt.pi"
-    mems = getData(source)
-    memd = getData(dest)
-    f = open(filename, "r",  encoding="utf8")
-    text = f.read()
-    f.close()
-    compressed = compressString(text, mems)
-    text = decompressString(compressed, memd)
-    f = open("translated_" + filename, "w",  encoding="utf8")
-    f.write(text)
-    f.close()    
-    
-def remike(filename):
-    small = "remike_en/mikerar1.pi"
-    large = "remike_en/mikerar2.pi"
-    memsmall = getData(small)
-    memlarge = getData(large)
-    f = open(filename, "r",  encoding="utf8")
-    text = f.read()
-    f.close()
-    compressed = compressString(text, memsmall)
-    text = decompressString(compressed, memlarge)
-    f = open("re" + filename, "w",  encoding="utf8")
-    f.write(text)
-    f.close()
-
 def test():
     for i in range(100):
         print(i, ntd(i), dtn(ntd(i)))
@@ -234,10 +241,10 @@ def inbetweeners():
     return " ,./()" + '"' +"':\n-"
 
 def lowercase():
-    return "abcdefghijklmnopqrstuvwxyzçáàéèíìóòúùãõâêîôû"
+    return "abcdefghijklmnopqrstuvwxyz"
 
 def uppercase():
-    return "ABCDEFGHIJKLMNOPQRSTUVWXYZÇÁÀÉÈÍÌÓÒÚÙÃÕÂÊÎÔÛ"
+    return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def numbers():
     return "0123456789"
@@ -252,394 +259,3 @@ def toLower(word):
     return new
     
 
-def decompress(filename):
-    newname = filename.split(".")[0] + ".mkt"
-    with gzip.open(filename, "rb") as f_in, open(newname, "wb") as f_out:
-        shutil.copyfileobj(f_in, f_out)
-    f = open(newname, "r", encoding="utf8") 
-    compressed = f.read()
-    f.close()
-    os.remove(newname)
-    newname2 = filename.split(".")[0] + ".txt"
-    text = decompressString(compressed)
-    f = open(newname2, "w",  encoding="utf8")
-    f.write(text)
-    f.close()
-
-def decompressString(string, mem = getData()):
-    print("decompressing")
-    settings = possibleSettings()
-    decompressed = ""
-    lastword = "%#,start"
-    jibber = ""
-    number = ""
-    st = ""
-    wordserial = ""
-    digitsleft = 0
-    capitalize = False
-    jibbering = False
-    numbering = False
-    wording = False    
-    for i in string:
-        if jibbering:
-            if i == strLiteral():
-                jibbering = False
-                decompressed = decompressed + jibber
-                jibber = ""
-            else:
-                jibber = jibber + i
-        elif numbering:
-            if i == intLiteral():
-                numbering = False
-                decompressed = decompressed + str(texttn(number))
-                number = ""
-            else:
-                number = number + i
-        elif wording:
-            wordserial = wordserial + i
-            digitsleft = digitsleft - 1
-            if digitsleft == 0:
-                wording = False
-                print("finding serial:",wordserial)
-                word = getWord(texttn(wordserial), st, lastword, capitalize, mem)
-                if word == None:
-                    print ("None Word ERROR!!! $$$$$$$$$$$$$$$$$$$$$$$$$$$$")
-                    word = word = mem._mainsort._list[0][0]
-                decompressed = decompressed + word
-                lastword = word
-                st = ""
-                wordserial = ""
-                digitsleft = 0
-                capitalize = False                  
-        else:
-            if i == strLiteral():
-                jibbering = True
-            if i == intLiteral():
-                numbering = True
-            if i in settings:
-                wording = True
-                sets = charSettings(i)
-                #SerialType: specific, general   
-                #Capitalize: no, yes
-                #Space: no, left, right
-                #Digit: no , . / ( ) " ' : ; - 
-                #SerialDigits: no 1 2 3                
-                if sets[0] == 0:
-                    st = "specific"
-                else:
-                    st = "general"
-                if sets[1] == 0:
-                    capitalize = False
-                else:
-                    capitalize = True
-                if sets[3] == 0:
-                    digit = ""
-                else:
-                    print (sets)
-                    print (i)
-                    digit = inbetweeners()[sets[3]]
-                if sets[2] == 0:
-                    decompressed = decompressed + digit
-                elif sets[2] == 1:
-                    decompressed = decompressed + " " + digit
-                else:
-                    decompressed = decompressed + digit + " "
-                digitsleft = sets[4]
-                if digitsleft == 0:
-                    wording = False
-                    print("                 zero serial word!")
-                    word = getWord(0, st, lastword, capitalize, mem)
-                    decompressed = decompressed + word
-                    lastword = word
-                    st = ""
-                    wordserial = ""
-                    digitsleft = 0
-                    capitalize = False                    
-    return decompressed
-            
-def getWord(wordserial, st, lastword, capitalize, mem):
-    print((wordserial, st, lastword, capitalize, mem))
-    if st == "specific":
-        if (wordserial < len(mem._wordsort[uncap(lastword)]._list)):
-            word = mem._wordsort[uncap(lastword)]._list[wordserial][0]
-        else:
-            print(wordserial, "out of index of", lastword, " #########################")
-            word = mem._wordsort[uncap(lastword)]._list[-1][0]
-    elif st == "general":
-        word = mem._mainsort._list[wordserial][0]
-    else:
-        raise ValueError ("invalid st:", st)
-    print("getting word:", word)
-    if capitalize:
-        if (word[0] in lowercase()):
-            return uppercase()[lowercase().index(word[0])] + word[1:]
-        else:
-            print("can't capitalize", word, " %%%%%%%%%%%%%%%%%%%%%%%%%")
-            return word
-    else:
-        return word
-
-def compress(filename, cut=False, size=0):
-    tim = time.clock()
-    delete = True
-    f = open(filename, "r", encoding='utf8')
-    text = f.read()
-    f.close()
-    if cut:
-        text = text[:size]
-    compressed = compressString(text)
-    newname = filename.split(".")[0] + ".mkt"
-    newname2 = filename.split(".")[0] + ".mkr"
-    f = open(newname, "w", encoding="utf8")
-    f.write(compressed)
-    f.close()
-    with open(newname, "rb") as f_in, gzip.open(newname2, "wb") as f_out:
-        shutil.copyfileobj(f_in, f_out)
-    if delete:
-        os.remove(newname)
-    tim2 = time.clock()
-    timename = filename.split(".")[0] + "_" +str(tim2-tim).split(".")[0] + ".txt"
-    print(timename)
-    f = open(timename, "w", encoding="utf8")
-    f.close()
-
-def compressString(string, mem = getData()):
-    compressed = ""
-    lastword = "%#,start"
-    thisword = ""
-    thisnumber = ""
-    inbetween = ""
-    lastjibber = ""
-    jibbering = True
-    numbering = False
-    wording = False
-    for i in string:
-        if jibbering:
-            if i in lowercase() + uppercase():
-                jibbering = False
-                if (len(lastjibber) >= 2):
-                    if lastjibber[-1] in inbetweeners():
-                        if (lastjibber[-2] in inbetweeners()) and ((lastjibber[-2] in inbetweeners()[1:]) != (lastjibber[-1] in inbetweeners()[1:])):
-                            inbetween = lastjibber[-2:]
-                            lastjibber = lastjibber[:-2]
-                        else:
-                            inbetween = lastjibber[-1:]
-                            lastjibber = lastjibber[:-1]
-                elif (len(lastjibber) == 1):
-                    if lastjibber in inbetweeners():
-                        inbetween = lastjibber
-                        lastjibber = ""
-                compressed = compressed + strLiteral() + lastjibber + strLiteral()
-                lastjibber = ""
-                wording = True
-                thisword = i
-            elif i in numbers():
-                jibbering = False
-                compressed = compressed + strLiteral() + lastjibber + strLiteral()
-                lastjibber = ""
-                numbering = True
-                thisnumber = i
-            else:
-                lastjibber = lastjibber + i
-        elif numbering:
-            if i in lowercase() + uppercase():
-                numbering = False
-                compressed = compressed + compressNumberString(thisnumber)   
-                thisnumber = ""
-                wording = True
-                thisword = i
-            elif i in numbers():
-                thisnumber = thisnumber + i
-            else:
-                numbering = False
-                compressed = compressed + compressNumberString(thisnumber)   
-                thisnumber = ""
-                jibbering = True
-                lastjibber = i
-        elif wording:
-            if i in lowercase() + uppercase():
-                thisword = thisword + i
-            elif i in numbers():
-                wording = False
-                res = compressWordString(lastword, inbetween, thisword, mem)
-                compressed = compressed + res[0]
-                if(res[1]):
-                    lastword = thisword
-                thisword = ""
-                inbetween = ""
-                numbering = True
-                thisnumber = i
-            else:
-                wording = False
-                res = compressWordString(lastword, inbetween, thisword, mem)
-                compressed = compressed + res[0]
-                if(res[1]):
-                    lastword = thisword
-                thisword = ""
-                inbetween = ""   
-                jibbering = True
-                lastjibber = i
-        else:
-            raise ValueError ("Invalid Booleans problem")
-    if wording:
-        compressed = compressed + compressWordString(lastword, inbetween, thisword, mem)[0]
-    elif numbering:
-        compressed = compressed + compressNumberString(thisnumber)
-    elif jibbering:
-        compressed = compressed + strLiteral() + lastjibber + strLiteral()
-    else:
-        raise ValueError ("Invalid Booleans problem 2")
-    print()
-    print(string)
-    print(compressed)
-    compressed = removeUnnecessaryLiterals(compressed)
-    print(compressed)
-    print()
-    return compressed
-
-def removeUnnecessaryLiterals(compressed):
-    lista = compressed.split(strLiteral()*2)
-    new1 = ""
-    for i in lista:
-        new1 = new1+i
-    lista = new1.split(intLiteral()*2)
-    new2 = ""
-    for i in lista:
-        new2 = new2+i
-    return new2
-
-def compressNumberString(string):
-    index = 0
-    while string[index] == "0":
-        index = index + 1
-        if index == len(string):
-            return strLiteral() + string + strLiteral()
-    zeroes = string[:index]
-    number = string[index:]
-    if len(number)<4:
-        compnum = strLiteral() + number + strLiteral()
-    else:
-        compnum = intLiteral() + nttext(eval(number)) + intLiteral()
-    return strLiteral() + zeroes + strLiteral() + compnum
-
-def compressWordString(lastword, inbetween, thisword, mem):
-    for i in thisword[1:]:
-        if i in uppercase():
-            return (strLiteral() + inbetween + thisword + strLiteral(), False)
-    general = mem._mainsort.findIndex(uncap(thisword))
-    if uncap(lastword) in list(mem._wordsort):
-        specific = mem._wordsort[uncap(lastword)].findIndex(uncap(thisword))
-    else:
-        specific = None
-       
-    if (general == None): #nao existe
-        print(thisword, "does not exist in data")
-        return (strLiteral() + inbetween + thisword + strLiteral(), False)
-    elif (specific == None): #so existe no general 
-        print(thisword, "does not exist in", lastword +"'s", "data")
-        return (createSettings(inbetween, thisword, general, "general"), True)
-    else: #existe no specific
-        print(thisword, "exists in", lastword +"'s", "data")
-        return (createSettings(inbetween, thisword, specific, "specific"), True)
-    
-def createSettings(inbetween, word, serialid, serialtype):
-    print((inbetween, word, serialid, serialtype))
-    code = nttext(serialid)
-    if (serialtype == "specific"):
-        serial = 0
-    elif (serialtype == "general"):
-        serial = 1
-    else:
-        raise ValueError(serialtype + "is not a valid serial type")
-    if (word[0] in lowercase()):
-        capt = 0
-    else:
-        capt = 1
-    if " " in inbetween:
-        if len(inbetween) == 1:
-            space = 1
-            digit = 0
-        elif inbetween[0] == " ":
-            space = 1
-            digit = inbetweeners().index(inbetween[1])
-        else:
-            space = 2
-            digit = inbetweeners().index(inbetween[0])
-    else:
-        space = 0
-        if len(inbetween) == 2:
-            raise ValueError("no space in two digit inbetween")
-        elif len(inbetween) == 1:
-            digit = inbetweeners().index(inbetween)
-        else:
-            digit = 0
-    sd = len(code)
-    print(settingsChar(serial, capt, space, digit, sd) + code)
-    return settingsChar(serial, capt, space, digit, sd) + code
-    
-#a Settings Character codes the following info about the upcoming word:
-#SerialType: specific, general   
-#Capitalize: no, yes
-#Space: no, left, right
-#Digit: no , . / ( ) " ' : \n - 
-#SerialDigits: no 1 2 3
-def settingsChar(serial, capt, space, digit, sd):
-    base = 166
-    a = 2
-    b = 2
-    c = 3
-    d = 11
-    e = 4
-    return chr(base + serial*b*c*d*e + capt*c*d*e + space*d*e + digit*e + sd)
-
-def charSettings(char):
-    base = 166
-    a = 2
-    b = 2
-    c = 3
-    d = 11
-    e = 4    
-    order = ord(char)
-    index = order - base
-    sd = index%e
-    index = index//e
-    digit = index%d
-    index = index//d
-    space = index%c
-    index = index//c
-    capt = index%b
-    index = index//b
-    serial = index
-    return (serial, capt, space, digit, sd)
-    
-def possibleSettings():
-    string = ""
-    for i in range(1+ ord(settingsChar(1, 1, 2, 10 ,3)) - ord(settingsChar(0, 0, 0, 0, 0))):
-        string = string + chr(ord(settingsChar(0, 0, 0, 0, 0))+i)
-    return string
-
-
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-def cutforexp(size):
-    filename = "exp/eng_news_2015_10K-sentences.txt"
-    f = open(filename, "r", encoding = "utf8")
-    text = f.read()
-    f.close()
-    text = text[:size]
-    newname = filename[:-4] + "_" + (6-len(str(size)))*"0" + str(size) + ".txt"
-    f = open(newname, "w", encoding = "utf8")
-    f.write(text)
-    f.close()
-    
-def bestseqs(count, leng = 8):
-    mem = getData()
-    for i in range(count):
-        first = mem._mainsort._list[i][0]
-        stri = first
-        last = first        
-        for j in range(leng):
-            new = mem._wordsort[last]._list[0][0]
-            last = new
-            stri = stri + " " + new
-        if not "the" in stri:
-            print (stri)
